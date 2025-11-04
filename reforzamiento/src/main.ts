f601a3f5b2abee3a286a6414692a9249a5ae4085
Edison Feria Snabria <edisonferia4@gmail.com>
2025-11-04 07:29:24 -0500

    Primer commit

---
+++ reforzamiento/src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+import './bases/04-arrays.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+  <h1>Hola Mundo</h1>
+  <p>Este es un parrafo para probar si se muestra mi parrafo</p>
+  </div>
+`
