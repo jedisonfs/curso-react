cb097ec7e76df9eeaf24658eb8f1e7c06686fea4
Edison Feria <edisonferia4@gmail.com>
2025-11-19 00:31:43 -0500

    [feat]: se crea mi primer componente react

---
+++ 02-first-steps/src/FirstStepApp.tsx
@@ -0,0 +1,14 @@
+export function FirstStepApp() {
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <p>Esto es un parrafo</p>
+        
+            <button>Click me</button>
+        
+            <div>
+              <h2>Hola Dentro de un div</h2>
+            </div>
+        </>
+    );
+}
--- 02-first-steps/src/main.tsx
+++ 02-first-steps/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
+import { FirstStepApp } from './FirstStepApp.tsx'
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
+/*
+Si se quita el StrictMode la aplicacion falla porque en react sin el stictmode solo permite una etiqueta html
+se debe regresar una sola etiqueta, se puede devolver un <div> </div> esto porque todo lo que tiene adentro
+cuando como parte de un dix es lo mas utilizado apra resolver este error
+*/
+
+  <StrictMode> // modo estricto de react para que se puedan detectar errores cuando se recarga la pagina
+    <FirstStepApp/> !-- Los framentos en react son capatilizados de esta forma funcionan-!
   </StrictMode>,
 )
