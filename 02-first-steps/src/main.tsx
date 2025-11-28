783dd4865715758de6664310470d98e6077c50bd
Edison Feria Sanabria <edisonferia4@gmail.com>
2025-11-27 19:05:56 -0500

    [feat]: se agregan comentarios correctamente

--- 02-first-steps/src/main.tsx
+++ 02-first-steps/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { FirstStepApp } from './FirstStepApp.tsx'
+import { MyAwesomeApp } from './MyAwesomeApp.tsx'
+
 createRoot(document.getElementById('root')!).render(
 /*
 Si se quita el StrictMode la aplicacion falla porque en react sin el stictmode solo permite una etiqueta html
@@ -8,7 +10,8 @@ se debe regresar una sola etiqueta, se puede devolver un <div> </div> esto porqu
 cuando como parte de un dix es lo mas utilizado apra resolver este error
 */
 
-  <StrictMode> // modo estricto de react para que se puedan detectar errores cuando se recarga la pagina
-    <FirstStepApp/> !-- Los framentos en react son capatilizados de esta forma funcionan-!
-  </StrictMode>,
+  <StrictMode> /*modo estricto de react para que se puedan detectar errores cuando se recarga la pagina */
+    <FirstStepApp/> /* Los framentos en react son capatilizados de esta forma funcionan */
+    <MyAwesomeApp/>
+  </StrictMode>
 )
